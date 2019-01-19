eb59e1504800e3f9cc84b1876ac0182cd2b7f103
Pawel Kaliniak <pawel.kaliniak@externals.adidas.com>
2019-01-19 21:50:00 +0100

    SSTI demo - user can upload own templates

---
+++ src/main/java/com/backendvulnerabilities/ssti/HelloController.java
@@ -0,0 +1,49 @@
+package com.backendvulnerabilities.ssti;
+
+import freemarker.cache.MultiTemplateLoader;
+import freemarker.cache.StringTemplateLoader;
+import freemarker.cache.TemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @Autowired
+    private  Configuration con;
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/hello")
+    public String hello(@RequestBody Map<String,Object> body, Model model) {
+        model.addAttribute("name", body.get("name"));
+        return "hello";
+    }
+
+    @RequestMapping(value = "/template", method =  RequestMethod.POST)
+    public String template(@RequestBody Map<String,String> templates) throws IOException {
+        StringTemplateLoader stringLoader = new StringTemplateLoader();
+        for(String templateKey : templates.keySet()){
+            stringLoader.putTemplate(templateKey, templates.get(templateKey));
+        }
+        con.setTemplateLoader(new MultiTemplateLoader(new TemplateLoader[]{stringLoader,
+            con.getTemplateLoader()}));
+        return "index";
+    }
+}
